e962665601da4b0966328903e7b782843fa15a29
RatherLogical <39105734+RatherLogical@users.noreply.github.com>
2020-12-14 19:23:58 -0500

    Fix api_base_url

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,5 +1,5 @@
 window.uptimeter_config = {
-    api_base_url: "https://status.dzlte.st", // The URL of the uptimeter backend installation (no trailing slash)
+    api_base_url: "https://status.example.com", // The URL of the uptimeter backend installation (no trailing slash)
     api_base_path: "", // The base path of the backend installation's public directory (no trailing slash)
     live_update: true, // Whether or not the status page should automatically check for new data
     live_update_interval: 60000, // How often the status page should check for new data
