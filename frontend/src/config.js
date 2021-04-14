fc185d88f803bdd849e7b32e8a78cf6748aeacf5
unknown <39105734+RatherLogical@users.noreply.github.com>
2021-04-13 20:18:08 -0400

    Update frontend deps to latest versions

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -3,7 +3,7 @@ window.uptimon_config = {
     page_title: "Uptimon", // The title of the page
     api_base_path: "api", // The base path of the backend installation's api directory (no leading or trailing slash).
     live_update: true, // Whether or not the status page should automatically check for new data.
-    live_update_interval: 60000, // How often the status page should check for new data.
+    live_update_interval: 60000, // How often the status page should check for new data (in milliseconds).
     shown_data_points: 25, // The amount of data points to show on the chart (if the database contains more data points than the value of 'shown_data_points' the data will be decimated to accurately display relative highs and lows.).
     online_primary_color: "#47d337", // Main color used when a service is online.
     online_secondary_color: "#3ab42d", // Accent color used when a service is online.
